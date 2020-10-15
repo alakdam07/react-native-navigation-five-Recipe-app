f39957a9694d28893f96c2fbec0520180f70773c
alak <alak@estatelogs.com>
2020-10-15 17:14:38 +0300

    ...

---
+++ screens/Details/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+interface Props {
+
+}
+
+
+
+export default function index() {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
---
+++ screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+interface Props {
+
+}
+
+export const index = (props: Props) => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
